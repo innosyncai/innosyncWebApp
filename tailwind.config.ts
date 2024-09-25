ae09b47b2800368d13eb530b23d1fa27fec2e9b6
innosyncai <innosyncai@gmail.com>
2024-09-25 21:39:16 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#0E1F33',
+        blue: {
+          300: '#52B6FE',
+          500: '#018BF7',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
